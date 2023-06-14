b2a9bb5582c19476fe0fa29baaca4b14c4f52bb2
erickwakyeboateng <boateng.eric44@gmail.com>
2023-06-14 19:27:48 +0000

    add best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-     """Returns the biggest integer value."""
+    """Returns the biggest integer value."""
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     restt = list(a_dictionary.keys())[0]
