7a34e3ed34edc1b437282b68161f1790b59e5b27
eric <boateng.eric44@gmail.com>
2023-09-16 09:51:00 +0000

    testing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,11 +3,46 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Parse command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        # Connect to MySQL server on localhost
+        db = MySQLdb.connect(
+            user=username,
+            passwd=password,
+            db=database,
+            host='localhost',
+            port=3306
+        )
+
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
+
+        # Execute the SQL query to retrieve states sorted by id
+        cursor.execute("SELECT * FROM states ORDER BY id")
+
+        # Fetch all the results
+        states = cursor.fetchall()
+
+        # Display the states
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("Error: {}".format(e))
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
